951e3b35498aea9e2dea15bd5719294f23ff877c
Manglesh Goyal <mangleshgoyal8@gmail.com>
2020-08-11 14:11:13 +0530

    resolving error

--- src/main/java/com/springboot/web/springbootfirstwebapplication/employeeProject/controller/EmployeeController.java
+++ src/main/java/com/springboot/web/springbootfirstwebapplication/employeeProject/controller/EmployeeController.java
@@ -9,11 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.web.springbootfirstwebapplication.employeeProject.dto.EmployeeDTO;
 import com.springboot.web.springbootfirstwebapplication.employeeProject.service.EmployeeSerivce;
 //import com.springboot.web.springbootfirstwebapplication.studentProject.service.StudentService;
 
+@RestController
 public class EmployeeController {
 
 	@Autowired
